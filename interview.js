0450360e55f8fa3ac52f0c4c5a39eb41d97941ef
Nazim8115 <nazim.nn459@gmail.com>
2023-11-21 15:29:23 +0530

    interviews and entities

---
+++ interview.js
@@ -0,0 +1,7 @@
+// question -1
+/*
+large Numerric literals are difficult for the human eye to parse quickly , espacially when there are lots of repeting digits. */
+
+const num1 = 100000;
+const num2 = 100_000;
+console.log(num1 === num2);
