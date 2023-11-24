0dd3eea07ee3f508756d9be88f17c02d497a68bb
Nazim8115 <nazim.nn459@gmail.com>
2023-11-24 18:44:34 +0530

    binary search using js

--- interview.js
+++ interview.js
@@ -2,6 +2,29 @@
 /*
 large Numerric literals are difficult for the human eye to parse quickly , espacially when there are lots of repeting digits. */
 
-const num1 = 100000;
-const num2 = 100_000;
-console.log(num1 === num2);
+// const num1 = 100000;
+// const num2 = 100_000;
+// console.log(num1 === num2);
+
+// write linear serch in javascript
+
+function binarySearch(arr) {
+  let low = 0;
+  let data = 1;
+  let high = arr.length;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === data) {
+      return mid;
+    } else if (arr[mid] < data) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(binarySearch(arr));
